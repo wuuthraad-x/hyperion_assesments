24ae5b6b83625945e6b35a0c59fa6d9b475ba5f6
wuuthraad <96783599+wuuthraad-x@users.noreply.github.com>
2023-03-25 12:05:43 +0200

    Update section_B_option_3.py
    In this test suite, we define the ohms and resist functions as provided in the question. We then define a TestResist class that inherits from unittest.TestCase and define a single test case test_resist that tests the resist function on a few different inputs. Each test case uses the assertAlmostEqual method to check that the result of the resist function is equal (within a small tolerance) to the expected result.
    
    To run the test suite on Windows, you can save the code above in a file called test_resist.py and run the following command in a terminal:
    "python -m unittest test_resist.py"
    
    This will run the test suite and display the results in the terminal.
    
    Regarding the worst-case space complexity of the resist function, the function uses regular expressions and recursive function calls to evaluate nested lists of resistors. The space complexity of the regular expression and recursive calls will depend on the depth of the nested lists, which could potentially be very large. Therefore, the worst-case space complexity of the resist function could be O(n) where n is the depth of the nested lists.

--- section_B_option_3.py
+++ section_B_option_3.py
@@ -1,14 +1,23 @@
-#installing re library
 !pip install re
+#importing nessecary libraries
+import unittest
 import re
 
 def ohms(s):
-  "function for calculating ohms"
     res = 1/sum(1/i for i in eval(s)) if s[0] == '[' else sum(eval(s))
     return str(res)
 
 def resist(net):
-  "function for getting resistance in circuit"
     while '(' in net or '[' in net:
         net = re.sub(r'[\[\(][\d,. ]+[\)\]]', lambda x: ohms(x.group()), net)
     return round(float(net), 1)
+
+class TestResist(unittest.TestCase):
+    def test_resist(self):
+        self.assertAlmostEqual(resist('(2,[3,6],2)'), 2.2)
+        self.assertAlmostEqual(resist('[10, [20, (30, 40)], 50]'), 16.7)
+        self.assertAlmostEqual(resist('[1, [2, [3, [4, [5, [6, 7]]]]]]'), 1.2)
+        self.assertAlmostEqual(resist('(2.5, [3.7, 4.5], 1.2, [5.3, (3.5, 2.2)])'), 1.4)
+
+if __name__ == '__main__':
+    unittest.main()
